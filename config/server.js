d7038e5774bfe7eb55e67789226925cb82473ea7
haroonahmad0 <haroon.ahmad@empg.com>
2021-04-05 18:40:07 +0500

    intinal commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8457dd65c95191679248179d29256c81'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0e0f4794-5c66-47ed-8cf2-8f6e7a37f657'
+};
